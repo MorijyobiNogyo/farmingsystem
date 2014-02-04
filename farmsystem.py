8eb9850faf45ade8c12510e24c635cdb5b9a8bca
鎌田司志 <kamatuka3@gmail.com>
2014-02-04 16:07:22 +0900

    farmingsystem.py追加
    今後これを編集する？

---
+++ farmsystem.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+class FarmingsystemMain(object):
+	"""docstring for FarmingsystemMain"""
+	def __init__(self, arg):
+		super(FarmingsystemMain, self).__init__()
+		self.arg = arg
+		
+class Temperature(object):
+	"""docstring for Temperature"""
+	def __init__(self, arg):
+		super(Temperature, self).__init__()
+		self.arg = arg
+		
+class Humidity(object):
+	"""docstring for Humidity"""
+	def __init__(self, arg):
+		super Humidity, self).__init__()
+		self.arg = arg
+		
