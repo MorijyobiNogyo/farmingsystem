82c8804742b8afacff185d8cf33197468b260fc2
鎌田司志 <kamatuka3@gmail.com>
2014-02-05 16:10:40 +0900

    farmsystem.pyの追加
    クラス等を追加しました。

--- farmsystem.py
+++ farmsystem.py
@@ -1,20 +1,70 @@
 # -*- coding: utf-8 -*-
 
 class FarmingsystemMain(object):
-	"""docstring for FarmingsystemMain"""
+	"""
+	農業システムの中心になるクラスです。
+	"""
 	def __init__(self, arg):
 		super(FarmingsystemMain, self).__init__()
 		self.arg = arg
 		
-class Temperature(object):
-	"""docstring for Temperature"""
+class TemperatureSensor(object):
+	"""
+	温度センサーに関するクラスです。
+
+	使用するGPIO:
+	"""
 	def __init__(self, arg):
-		super(Temperature, self).__init__()
+		super(TemperatureSensor, self).__init__()
 		self.arg = arg
 		
-class Humidity(object):
-	"""docstring for Humidity"""
+class HumiditySensor(object):
+	"""
+	湿度センサーに関するクラスです。
+
+	使用するGPIO:
+	"""
 	def __init__(self, arg):
-		super Humidity, self).__init__()
+		super HumiditySensor, self).__init__()
+		self.arg = arg
+		
+class OpticalSensor(object):
+	"""
+	光センサーに関するクラスです。
+
+	使用するGPIO:
+	"""
+	def __init__(self, arg):
+		super(OpticalSensor, self).__init__()
+		self.arg = arg
+		
+class CameraSensor(object):
+	"""
+	カメラに関するクラスです。
+
+	使用するGPIO:
+	"""
+	def __init__(self, arg):
+		super(CameraSensor, self).__init__()
+		self.arg = arg
+
+class WaterPump(object):
+	"""
+	ポンプに関するクラスです。
+
+	使用するGPIO:
+	"""
+	def __init__(self, arg):
+		super(WaterPunp, self).__init__()
+		self.arg = arg
+		
+class Bulb(object):
+	"""
+	電球に関するクラスです。
+
+	使用するGPIO:
+	"""
+	def __init__(self, arg):
+		super(Bulb, self).__init__()
 		self.arg = arg
 		
