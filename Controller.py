de7fbeabf305d59e39dea25617f44b0d5fcc16cd
鎌田司志 <kamatuka3@gmail.com>
2014-03-24 11:48:23 +0900

    Controller.pyのコード追加
    Controllerクラスを実装したWateringControllerクラス・LightingControllerクラスを形だけ追加しました。

--- Controller.py
+++ Controller.py
@@ -3,15 +3,18 @@
 
 #モジュールのインポート
 import abc
+#Raspberry Piで使うときは解除する
+#import RPi.GPIO as GPIO
 
 
 #以下コード
 
 class Controller(object, metaclass=abc.ABCMeta):
-	"""その他のコントローラの基底クラス。"""
-	def __init__(self, arg):
-		super(Controller, self).__init__()
-		self.arg = arg
+
+
+	"""コントローラの基底クラス。"""
+	def __init__(self):
+		pass
 
 	@abc.abstractmethod
 	def powerOn():
@@ -22,4 +25,29 @@ class Controller(object, metaclass=abc.ABCMeta):
 	def PowerOff():
 		"""機器をオフにするメソッド。"""
 		pass
+
+class WateringController(Controller):
+	"""ポンプを制御するクラス。"""
+	def __init__(self):
+		pass
 		
+	def powerOn(self):
+		"""ポンプをオンにするメソッド"""
+		pass
+
+	def PowerOff(self):
+		"""ポンプをオフにするメソッド"""
+		pass
+
+class LightingController(Controller):
+	"""明かりを制御するクラス"""
+	def __init__(self):
+		pass
+
+	def powerOn(self):
+		"""明かりをつけるメソッド"""
+		pass
+
+	def PowerOff(self):
+		"""明かりを消すメソッド"""
+		pass
