d780938e63a4944f7255dcdff0374a0f1656bd68
aaaaaaai <arakawa85@gmail.com>
2014-03-28 12:25:16 +0900

    Sensor.pyのコード追加

--- Sensor.py
+++ Sensor.py
@@ -3,3 +3,21 @@ import MFS1Info
 import Readable
 
 class Sensor(Readable, MFS1Info, __metaclass__ = abc.ABCMeta):
+	@abc.abstractmethod
+	def get():
+		pass
+
+# 温度の値読込むクラス
+class LightSensor(Sensor):
+	def get():
+		pass
+
+# 湿度の値読込むクラス
+class ThermohygrometerSensor(Sensor):
+	def get():
+		pass
+
+# 画像読込むクラス
+class ImageSensor(Sensor):
+	def get():
+		pass
